30bea5a8c218406516e37886ff4493cfdbda730f
Federico Iosue <f@federicoiosue.it>
2012-11-13 12:06:45 +0100

    Nuova classe per la creazione di mappe ordinate da stringhe

---
+++ Libs/src/it/autostrade/feio/utils/obj/MapSplitter.java
@@ -0,0 +1,34 @@
+package it.autostrade.feio.utils.obj;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class MapSplitter extends Object {
+
+	private static String objectSeparator;
+	private static String keyValueSeparator;
+
+	public static MapSplitter on(String objectSeparator) {
+		MapSplitter.objectSeparator = objectSeparator;
+		return null;
+	}
+
+	public static MapSplitter withKeyValueSeparator(String keyValueSeparator) {
+		MapSplitter.keyValueSeparator = keyValueSeparator;
+		return null;
+	}
+
+	public static Map<String, String> split(CharSequence s) {
+		Map<String, String> map = new LinkedHashMap<String, String>();
+
+		String[] a = s.toString().split(objectSeparator);
+
+		for (int i = a.length - 1; i >= 0; i--) {
+			String[] array = a[i].split(keyValueSeparator);
+			map.put(array[0], array[1]);
+		}
+		return map;
+	}
+
+
+}
